cbd72dd69f2b8e138f48ee900539495f943d9c18
SuperPox <calebkohl@gmail.com>
2021-03-02 21:23:01 -0700

    form sends data to controller which makes nested instances

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -37,29 +37,118 @@ class PollsController < ApplicationController
             poll.save
         end
 
-
-        binding.pry
-
         contentV = []
         if (params[:pa1] != nil)
             contentV << params[:pa1]
         end
         if (params[:pa2] != nil)
+            contentV << params[:pa2]
+        end
+        if (params[:pa3] != nil)
             contentV << params[:pa3]
         end
         if (params[:pa4] != nil)
             contentV << params[:pa4]
         end
-        if (params[:pa4] != nil)
-            contentV << params[:pa4]
-        end
-        contentV.to_s.gsub("\"", "")  #=> ["cat", "dog", "bird", "other"]
+        contentV.to_s.gsub("\"", "")  #=> ["cat", "dog", "bird", "other"]     
         contentV.each do |i|
             possible_answer_hash = {content: i}
-            possible_answer_instance = PossibleAnswer.create(possible_answer_hash)
-            question.possible_answers << possible_answer_instance      
-            question.save
-            poll.questions << question
+            possible_answer_instance = PossibleAnswer.create(possible_answer_hash)       
+            poll.questions[0].possible_answers << possible_answer_instance      
+            poll.questions[0].save
+            poll.questions << poll.questions[0]
+            poll.save
+        end
+
+        q2_pa_contentV = []
+        if (params[:pa5] != nil)
+            q2_pa_contentV << params[:pa5]
+        end
+        if (params[:pa6] != nil)
+            q2_pa_contentV << params[:pa6]
+        end
+        if (params[:pa7] != nil)
+            q2_pa_contentV << params[:pa7]
+        end
+        if (params[:pa8] != nil)
+            q2_pa_contentV << params[:pa8]
+        end
+        q2_pa_contentV.to_s.gsub("\"", "")  #=> ["cat", "dog", "bird", "other"]     
+        q2_pa_contentV.each do |i|
+            possible_answer_hash = {content: i}
+            possible_answer_instance = PossibleAnswer.create(possible_answer_hash)     
+            poll.questions[1].possible_answers << possible_answer_instance      
+            poll.questions[1].save
+            poll.questions << poll.questions[1]
+            poll.save
+        end
+
+        q3_pa_contentV = []
+        if (params[:pa9] != nil)
+            q3_pa_contentV << params[:pa9]
+        end
+        if (params[:pa10] != nil)
+            q3_pa_contentV << params[:pa10]
+        end
+        if (params[:pa11] != nil)
+            q3_pa_contentV << params[:pa11]
+        end
+        if (params[:pa12] != nil)
+            q3_pa_contentV << params[:pa12]
+        end
+        q3_pa_contentV.to_s.gsub("\"", "")  #=> ["cat", "dog", "bird", "other"]     
+        q3_pa_contentV.each do |i|
+            possible_answer_hash = {content: i}
+            possible_answer_instance = PossibleAnswer.create(possible_answer_hash)        
+            poll.questions[2].possible_answers << possible_answer_instance      
+            poll.questions[2].save
+            poll.questions << poll.questions[2]
+            poll.save
+        end
+
+        q4_pa_contentV = []
+        if (params[:pa13] != nil)
+            q4_pa_contentV << params[:pa13]
+        end
+        if (params[:pa14] != nil)
+            q4_pa_contentV << params[:pa14]
+        end
+        if (params[:pa15] != nil)
+            q4_pa_contentV << params[:pa15]
+        end
+        if (params[:pa16] != nil)
+            q4_pa_contentV << params[:pa16]
+        end
+        q4_pa_contentV.to_s.gsub("\"", "")  #=> ["cat", "dog", "bird", "other"]     
+        q4_pa_contentV.each do |i|
+            possible_answer_hash = {content: i}
+            possible_answer_instance = PossibleAnswer.create(possible_answer_hash)        
+            poll.questions[3].possible_answers << possible_answer_instance      
+            poll.questions[3].save
+            poll.questions << poll.questions[3]
+            poll.save
+        end
+
+        q5_pa_contentV = []
+        if (params[:pa17] != nil)
+            q5_pa_contentV << params[:pa17]
+        end
+        if (params[:pa18] != nil)
+            q5_pa_contentV << params[:pa18]
+        end
+        if (params[:pa19] != nil)
+            q5_pa_contentV << params[:pa19]
+        end
+        if (params[:pa20] != nil)
+            q5_pa_contentV << params[:pa20]
+        end
+        q5_pa_contentV.to_s.gsub("\"", "")  #=> ["cat", "dog", "bird", "other"]     
+        q5_pa_contentV.each do |i|
+            possible_answer_hash = {content: i}
+            possible_answer_instance = PossibleAnswer.create(possible_answer_hash)        
+            poll.questions[4].possible_answers << possible_answer_instance      
+            poll.questions[4].save
+            poll.questions << poll.questions[4]
             poll.save
         end
 
